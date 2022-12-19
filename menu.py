d4e07907b78b23a67697397a13a606acebaf246c
Reuven Lerner <reuven@lerner.co.il>
2022-12-19 20:45:56 +0200

    gitautosync update 45

--- menu.py
+++ menu.py
@@ -4,7 +4,7 @@
 def menu(strings):                  # start the function definition
 
     # ask the user to enter a string from "strings", a list we got from the caller
-    user_choice = input(f'Enter one of {strings}').strip()
+    user_choice = input(f'Enter one of {strings}').strip()   # strip removes whitespace from either side of a string
     
     if user_choice in strings:    # if the user's choice (a string) is an element of strings, that means
         return user_choice        # we got ligitimate input, and we can return it as output
